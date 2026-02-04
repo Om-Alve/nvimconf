a6efed27941745a3aa03c1079e94e7386bd859e2
Om Alve <omalve8174@gmail.com>
2026-02-04 11:23:32 +0530

    chore: update avante model

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,6 +3,10 @@ return {
   event = "VeryLazy",
   opts = {
     -- add any options here
+    lsp = {
+      hover = { enabled = false },
+      signature = { enabled = false },
+    },
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
